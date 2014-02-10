a9aa407538818385cc9fa181244ed670cd9ec986
PopManOO <laboitafred@free.fr>
2014-02-10 20:13:59 +0100

    test launch activity

--- stat/src/com/mycompany/myapp/MainActivity.java
+++ stat/src/com/mycompany/myapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.content.*;
 
 public class MainActivity extends Activity
 {
@@ -12,6 +13,14 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.formation_442);	
+		Button g = (Button) findViewById(R.id.btnGDB);
+			
+		g.setOnClickListener(new View.OnClickListener(){		
+				public void onClick(View p1){
+					Intent i = new Intent(MainActivity.this, action.class);
+					startActivity(i);
+				}
+		});
     }
 }
---
+++ stat/src/com/mycompany/myapp/action.java
@@ -0,0 +1,20 @@
+package com.mycompany.myapp;
+
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.content.*;
+
+public class action extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.trame);
+	}
+}
